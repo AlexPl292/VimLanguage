d5cc109017bd67b42289eb49dd8dcfcc172685d7
Alex Plate <AlexPl292@gmail.com>
2019-10-29 18:05:53 +0300

    Possibility to get the command name

---
+++ src/test/kotlin/dev/feedforward/vim/lang/VimTest.kt
@@ -0,0 +1,19 @@
+package dev.feedforward.vim.lang
+
+import com.intellij.psi.PsiFileFactory
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import dev.feedforward.vim.lang.psi.VimCommand
+import junit.framework.TestCase
+
+class VimTest : BasePlatformTestCase() {
+    fun `test parser`() {
+        val elements = PsiFileFactory.getInstance(project).createFileFromText("x.vim", VimFileType, """
+            ascii
+            only
+        """.trimIndent())
+        val child = elements.firstChild
+        val commandName = (child as VimCommand).commandName
+        TestCase.assertEquals("ascii", commandName)
+    }
+}
